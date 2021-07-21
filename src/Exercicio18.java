30a8b0dc97b537ea4da57ab18d5d89677d9b5af1
PGMDDuardo <87023026+PGMDDuardo@users.noreply.github.com>
2021-07-21 08:37:49 -0300

    Lsta1 parte2

---
+++ src/Exercicio18.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Exercicio18 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Entre com a tamanho do arquivo:");
+        double tamArquivo = scan.nextDouble();
+
+        System.out.println("Entre com a velocidade da internet:");
+        double velInternet = scan.nextDouble();
+
+        double tempo = tamArquivo / velInternet;
+
+        System.out.println(" Tempo de downloand:" + tempo);
+    }
+}
